00d631e3356adc1249005860f527269d4351fe0c
Richard Clark <rclark@c2i.fr>
2017-09-12 10:27:49 +0200

    Gestion asynchrone dans l'api Persons

--- server/WebDrh/Controllers/PersonsController.cs
+++ server/WebDrh/Controllers/PersonsController.cs
@@ -1,18 +1,19 @@
-﻿using System.Web.Http;
+﻿using System.Threading.Tasks;
+using System.Web.Http;
 using WebDrh.Models;
 
 namespace WebDrh.Controllers
 {
     public class PersonsController : ApiController
     {
-        public IHttpActionResult Get()
+        public Task<IHttpActionResult> Get()
         {
-            return Ok(new[]
+            return Task.FromResult<IHttpActionResult>(Ok(new[]
             {
                 new PersonModel {FirstName = "Richard", LastName = "Clark", Id = 1},
                 new PersonModel {FirstName = "Denis", LastName = "Voituron", Id = 2},
                 new PersonModel {FirstName = "Christophe", LastName = "Peugnet", Id = 4}
-            });
+            }));
         }
     }
 }
