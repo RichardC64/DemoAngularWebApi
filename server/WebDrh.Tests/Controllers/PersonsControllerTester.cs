5cdcfda0dd1dad060f23811e9de0a2db00da8f7b
Richard Clark <rclark@c2i.fr>
2017-09-12 11:45:49 +0200

    Ajout test unitaire

---
+++ server/WebDrh.Tests/Controllers/PersonsControllerTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebDrh.Controllers;
+using WebDrh.Models;
+
+namespace WebDrh.Tests.Controllers
+{
+    [TestClass]
+    public class PersonsControllerTester
+    {
+        [TestMethod]
+        public void ReturnOk()
+        {
+            var ctrl = new PersonsController();
+
+            var result = ctrl.Get().GetAwaiter().GetResult()as OkNegotiatedContentResult<PersonModel[]>;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Content);
+        }
+    }
+}
